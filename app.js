ff4a6e0dbf1cb1e13cd67b94452660bb1c7e7b84
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-02-28 18:03:08 +0545

    project added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const main = document.querySelector(".project-grid");
+const main = document.querySelectorAll(".project-grid");
 
 const projects = [
+  // Easy
   {
     mode: "easy",
     title: "Form Validation",
@@ -15,15 +16,188 @@ const projects = [
   },
   {
     mode: "easy",
-    title: "Google Maps Clone Very Long Title ",
+    title: "Google Maps Clone",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Analog Clock",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Impossible To Click Button",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Simple Tabs",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Rock Paper Scissors Game",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Text To Speech",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Eye Tracking Program",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Expandable Content",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "easy",
+    title: "Modal",
+    image: "",
+    codeLink: "",
+  },
+  // Medium
+  {
+    mode: "medium",
+    title: "Password Strength Tester",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Password Generator",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Quiz App",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Card Game",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Snake Game",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "AI Face Detection",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "AI Face Recognition",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Thanos Snap Effect",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Sortable Drag And Drop",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Speed Typing Game",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Tic Tac Toe",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Browser Based Guitar Amp",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "medium",
+    title: "Text Adventure Game",
+    image: "",
+    codeLink: "",
+  },
+  // Hard
+  {
+    mode: "hard",
+    title: "Custom Select Dropdown",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "hard",
+    title: "Calculator",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "hard",
+    title: "Card Matching Game",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "hard",
+    title: "Advanced Todo List",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "hard",
+    title: "Mars Weather App",
+    image: "",
+    codeLink: "",
+  },
+  {
+    mode: "hard",
+    title: "Battleship Clone",
     image: "",
     codeLink: "",
   },
 ];
 
-projects.forEach(
-  (project) =>
-    (main.innerHTML += `
+const easyProjects = projects.filter((project) => project.mode === "easy");
+
+var currentElement = main[0];
+projects.forEach((project) => {
+  if (project.mode === "medium") {
+    currentElement = main[1];
+    console.log("medium");
+  }
+  if (project.mode === "hard") {
+    currentElement = main[2];
+  }
+  currentElement.innerHTML += `
     <div class="project">
       <figure class="project-img">
         <img src="${
@@ -35,5 +209,5 @@ projects.forEach(
       <h4 class="project-name">${project.title}</h4>
       <p class="project-category">${project.mode.toUpperCase()}</p>
     </div>
-`)
-);
+`;
+});
